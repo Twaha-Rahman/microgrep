de73dc3f2613b16c5721536a13bab57b9e5e52ff
Twaha Rahman <mahi6703890@gmail.com>
2020-10-03 14:08:01 +0600

    Add time elasped feature

--- microgrep/src/main.rs
+++ microgrep/src/main.rs
@@ -1,5 +1,20 @@
+use std::process;
+use std::time::Instant;
+
 use microgrep::run;
 
 fn main() {
-    run();
+    let execution_start_time = Instant::now();
+
+    let result = run();
+
+    let elapsed = execution_start_time.elapsed();
+
+    match result {
+        Ok(_) => println!("\nTook {}ms to search.", elapsed.as_millis()),
+        Err(reason) => {
+            eprintln!("{}", reason);
+            process::exit(1);
+        }
+    }
 }
